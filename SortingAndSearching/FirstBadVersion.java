8302a12317997eaa0adb7c0d81582f1a087251ec
Diego Garcia <38167772+gciadiego@users.noreply.github.com>
2021-07-23 11:12:19 -0500

    First Bad Version Solution
    You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
    Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
    You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.

---
+++ SortingAndSearching/FirstBadVersion.java
@@ -0,0 +1,34 @@
+//First Bad Version
+//You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+//Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+//You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+public class FirstBadVersion{
+   public boolean isBadVersion(int num){
+      if(num == 4)
+         return true;
+      else
+         return false;
+   }
+   
+   public int firstBadVersion(int left, int right){
+      int middle = left + (right - left) / 2;
+      
+      if(left == middle && isBadVersion(middle))
+         return middle;
+      
+      if(isBadVersion(middle))
+         middle = firstBadVersion(left, middle);
+      else
+         middle = firstBadVersion(middle+1, right);
+         
+      return middle;
+   }
+   
+   public static void main(String[] args){
+      FirstBadVersion fbv = new FirstBadVersion();
+      int n = 5;
+      
+      System.out.println(fbv.firstBadVersion(1, n));
+   }
+}
